37a1fdb05c96a1da30b0813a0b6136a3569b83e5
DivthesignDevLab <daniel.verdu@e.craftww.com>
2021-03-03 13:21:45 +0100

    repo pre configuration

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/ts/index',
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: "html-loader",
+                        options: {minimize: true}
+                    }
+                ]
+            },
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+            },
+            {
+                test:/\.scss$/,
+                use:['style-loader', 'css-loader', 'sass-loader'],
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+            filename: "./index.html"
+        }),
+    ],
+    resolve: {
+        extensions: [
+            ".ts"
+        ]
+    },
+    output: {
+        filename: 'js/index.js',
+        path: path.resolve(__dirname, './build'),
+    }
+};
