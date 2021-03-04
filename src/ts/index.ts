1b46c818868ce8fce22cfe33b46c7382f865e373
DivthesignDevLab <daniel.verdu@e.craftww.com>
2021-03-04 08:38:23 +0100

    main branch clean

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,11 +1,2 @@
 import '../scss/style.scss';
-import { TransportFactory, TransportType } from './patterns/creational-patterns/factory-method';
 
-// Factory Method
-const camion = TransportFactory.create(TransportType.Land);
-const barco = TransportFactory.create(TransportType.Sea);
-const avion = TransportFactory.create(TransportType.Air);
-
-camion.createTransport();
-barco.createTransport();
-avion.createTransport();
