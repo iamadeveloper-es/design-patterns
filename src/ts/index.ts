b3692ecd56a2d8fa392013d0b80855d3abdb43bf
DivthesignDevLab <daniel.verdu@e.craftww.com>
2021-03-12 19:47:43 +0100

    builder pattern

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,5 +1,6 @@
 import '../scss/style.scss';
 import { Client, VictorianFactory, ModernFactory, ArtDecoFactory} from './patterns/creational-patterns/abstract-factory';
+import {CarBuilderDirector, FerrariCarBuilder, MercedesCarBuilder, BMWCarBuilder } from './patterns/creational-patterns/builder';
 import { UserFactory, UserType } from './patterns/creational-patterns/factory-method';
 
 
@@ -20,4 +21,21 @@ let artDecoTable = new Client().createTable(new ArtDecoFactory());
 console.log(victorianChair.info());
 console.log(modernSofa.info());
 console.log(artDecoTable.info());
+
+//Builder
+const ferrari = new FerrariCarBuilder();
+const mercedes = new MercedesCarBuilder();
+const bmw = new BMWCarBuilder();
+
+const ferrariDirector = new CarBuilderDirector(ferrari);
+const mercedesDirector = new CarBuilderDirector(mercedes);
+const bmwDirector = new CarBuilderDirector(bmw);
+
+ferrariDirector.build();
+mercedesDirector.build();
+bmwDirector.build();
+
+console.log(ferrariDirector.getCar());
+console.log(mercedesDirector.getCar());
+console.log(bmwDirector.getCar());
 
